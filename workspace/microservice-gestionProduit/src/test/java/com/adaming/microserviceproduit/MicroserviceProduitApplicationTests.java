345c4110a1b5b407a90a0b723c27a3707276bafe
unknown <prat.louise@protonmail.com>
2020-11-24 12:30:10 +0100

    my first commit

---
+++ workspace/microservice-gestionProduit/src/test/java/com/adaming/microserviceproduit/MicroserviceProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaming.microserviceproduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
