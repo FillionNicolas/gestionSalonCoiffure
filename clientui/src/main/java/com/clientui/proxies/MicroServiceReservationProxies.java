c4714c6adc2e819d76154d4cf92b06ef47ed372b
Sachet <adaming.laurent@gmail.com>
2020-11-25 15:35:31 +0100

    integration ms rdv complet

--- clientui/src/main/java/com/clientui/proxies/MicroServiceReservationProxies.java
+++ clientui/src/main/java/com/clientui/proxies/MicroServiceReservationProxies.java
@@ -22,16 +22,16 @@ public interface MicroServiceReservationProxies {
 	@GetMapping(value="/reservations")
     List<ReservationBean> findAllReservation();
 
-	@GetMapping("/reservations/{id}")
+	@GetMapping(value="/reservations/{id}")
 	public ReservationBean findOneReservation(@PathVariable("id") Long id);
 
     @PostMapping("/reservations")
 	public ReservationBean saveReservation(@RequestBody ReservationBean reservation);
 
-    @PutMapping("/reservations/{id}")
+    @PutMapping(value="/reservations/{id}")
   	public ReservationBean updateReservation(@RequestBody ReservationBean reservation, @PathVariable("id") Long id);
 
-  	@DeleteMapping("/reservations/{id}")
+  	@DeleteMapping(value="/reservations/{id}")
   	void deleteReservation(@PathVariable("id") Long id);
   	
   	
@@ -40,16 +40,16 @@ public interface MicroServiceReservationProxies {
   	@GetMapping(value="/prestations")
     List<PrestationBean> findAllPrestation();
 
-	@GetMapping("/prestations/{id}")
+	@GetMapping(value="/prestations/{id}")
 	public PrestationBean findOnePrestation(@PathVariable("id") Long id);
 
-    @PostMapping("/prestations")
+    @PostMapping(value="/prestations")
 	public PrestationBean savePrestation(@RequestBody PrestationBean prestation);
 
-    @PutMapping("/prestations/{id}")
+    @PutMapping(value="/prestations/{id}")
 	public PrestationBean updatePrestation(@RequestBody PrestationBean prestation, @PathVariable("id") Long id);
 
-	@DeleteMapping("/prestations/{id}")
+	@DeleteMapping(value="/prestations/{id}")
 	void deletePrestation(@PathVariable("id") Long id);
 	
 	
@@ -57,16 +57,16 @@ public interface MicroServiceReservationProxies {
 	@GetMapping(value="/salons")
     List<SalonBean> findAllSalon();
 
-	@GetMapping("/salons/{id}")
+	@GetMapping(value="/salons/{id}")
 	public SalonBean findOneSalon(@PathVariable("id") Long id);
 
-    @PostMapping("/salons")
+    @PostMapping(value="/salons")
 	public SalonBean saveSalon(@RequestBody SalonBean salon);
 
-    @PutMapping("/salons/{id}")
+    @PutMapping(value="/salons/{id}")
 	public SalonBean updateSalon(@RequestBody SalonBean salon, @PathVariable("id") Long id);
 
-	@DeleteMapping("/salons/{id}")
+	@DeleteMapping(value="/salons/{id}")
 	void deleteSalon(@PathVariable("id") Long id);
 
 }
