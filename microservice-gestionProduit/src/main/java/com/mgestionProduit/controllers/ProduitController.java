c2f047a4ab8a049f34b29cfee9b61bce45668942
unknown <prat.louise@protonmail.com>
2020-11-25 11:18:34 +0100

    ajout DTO pour gestion_Produit

--- microservice-gestionProduit/src/main/java/com/mgestionProduit/controllers/ProduitController.java
+++ microservice-gestionProduit/src/main/java/com/mgestionProduit/controllers/ProduitController.java
@@ -1,6 +1,7 @@
 package com.mgestionProduit.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mgestionProduit.dto.ProduitDTO;
 import com.mgestionProduit.entites.Produit;
+import com.mgestionProduit.mapper.IProduitMapper;
 import com.mgestionProduit.services.IProduitService;
 
 
@@ -22,31 +25,36 @@ import com.mgestionProduit.services.IProduitService;
 public class ProduitController {
 	
 	@Autowired
+	IProduitMapper produitMapper;
+	
+	@Autowired
 	IProduitService produitService;
 	
 	@GetMapping("/produits")
-	public List<Produit> findAll(){
-		return produitService.getProduits();
+	public List<ProduitDTO> findAll(){
+		return (List<ProduitDTO>)produitService.getProduits().stream().map(e->
+		produitMapper.convertToProduitDTO(e)).collect(Collectors.toList());
+				
 	}
 	
 	@GetMapping("/produits/{id}")
-	public Produit findOne(@PathVariable("id") Long id) {
-		return produitService.getProduit(id);
+	public ProduitDTO findOne(@PathVariable("id") Long id) {
+		return produitMapper.convertToProduitDTO(produitService.getProduit(id));
 		
 	}
 	
 	@PostMapping("/produits")
-	public Produit saveProduit(@RequestBody Produit produit) {
-		return produitService.saveProduit(produit);
+	public ProduitDTO saveProduit(@RequestBody Produit produit) {
+		return produitMapper.convertToProduitDTO(produitService.saveProduit(produit));
 	}
 	
 	@PutMapping("/produits/{id}")
-	public Produit updateProduit(@RequestBody Produit produit, @PathVariable("id")Long id ) {
+	public ProduitDTO updateProduit(@RequestBody Produit produit, @PathVariable("id")Long id ) {
 		Produit currentProduit = produitService.getProduit(id);
 		currentProduit.setNom(produit.getNom());
 		currentProduit.setPrix(produit.getPrix());
 		currentProduit.setCommande(produit.getCommande());
-		return produitService.saveProduit(currentProduit);
+		return produitMapper.convertToProduitDTO(produitService.saveProduit(currentProduit));
 	}
 	
 	@DeleteMapping("/produits/{id}")
